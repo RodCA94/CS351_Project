10cab534714c130fbfb030ddc9e95f2e8732159c
rodca94 <rodriguezsi760@gmail.com>
2026-02-18 22:35:44 -0800

    keyword search function complete

--- main.py
+++ main.py
@@ -3,29 +3,46 @@
 # Christian Rodriguez
 #
 
-#import re # regex import
+import re
 
-# token definition
-#put NamedTuple inside token
-class Token():
-	type: str
-	value: str
+#Token Defenitions
+keywords = [r"^(if)\s", r"^(else)\s", r"^(int)\s", r"^(float)\s"]
+integers = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
+operators = ["=", "+", ">", "*"]
+seperators = ["(", ")", ":", "\"", ";"]
 
 # TinyPie function
-def TinyPie(string1):
-	keywords: {"if"}	#type defintions here
-	integers: {"1", "2", "3", "4", "5", "6", "7", "8", "9"}
-	operator: {"*"}
-	seperator: {")"}
+def CutOneLineTokens(s):
+	print("Test input string:", s)
+	key = ""
+	for k in keywords:
+		m = re.search(k, s) # look for a match
+		if m:
+			key = m.group(1)
+			s = re.sub(k, "", s)
+			break  # breaks and exits look when a key is found
+
+	# Error detection if no keywords are found
+	if (key == ""):
+		print("Lexical error in input string:", s)
+		return
+
+	print("key:", key)
 
 # start of main area
-str1 = "int A1 = 5"
+str1 = "int A1=5"
 str2 = "float BBB2 =1034.2"
 str3 = "float cresult = A1 +BBB2 * BBB2"
 str4 = "if (cresult >10):"
 str5 = "     print(“TinyPie   “)"
 
-strs = [str1, str2, str3, str4, str5]
+#strs = [str1, str2, str3, str4, str5]
 
-for s in strs:
-	print(s)
+#for s in strs:
+	#print(s)
+
+CutOneLineTokens(str1)
+CutOneLineTokens(str2)
+CutOneLineTokens(str3)
+CutOneLineTokens(str4)
+CutOneLineTokens(str5)
