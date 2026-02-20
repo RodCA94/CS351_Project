a96dbf86d31ed20d41f9437ae28145bf13abc579
rodca94 <rodriguezsi760@gmail.com>
2026-02-19 22:17:10 -0800

    keyword search function complete

--- main.py
+++ main.py
@@ -6,7 +6,7 @@
 import re
 
 #Token Defenitions
-keywords = [r"^(if)\s", r"^(else)\s", r"^(int)\s", r"^(float)\s"]
+keywords = [r"(if)\s", r"(else)\s", r"(int)\s", r"(float)\s"]
 integers = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
 operators = ["=", "+", ">", "*"]
 seperators = ["(", ")", ":", "\"", ";"]
@@ -14,17 +14,35 @@ seperators = ["(", ")", ":", "\"", ";"]
 # TinyPie function
 def CutOneLineTokens(s):
 	print("Test input string:", s)
+
+	#Validate only 1 keyword
+	keywordCount = 0
+	for i in keywords:
+		count = len(re.findall(i,s))
+		keywordCount += count
+		if keywordCount > 1:
+			print("Invalid Syntax: Multiple Keywords")
+			return
+
+	# store tokens
 	key = ""
+	operatorToken = []
+	seperatorToken = []
+	identifierToken = []
+	intToken = []
+	floatToken = []
+	strToken = []
+
+	# Removes the keyword found in line
 	for k in keywords:
 		m = re.search(k, s) # look for a match
 		if m:
 			key = m.group(1)
 			s = re.sub(k, "", s)
-			break  # breaks and exits look when a key is found
 
 	# Error detection if no keywords are found
 	if (key == ""):
-		print("Lexical error in input string:", s)
+		print("Invalid Syntax: No Keywords Found")
 		return
 
 	print("key:", key)
